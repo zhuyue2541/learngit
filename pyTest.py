eccd8dabb0a1d5cded548624137020899f9508a3
zhuyue <zhuyue2541@outlook.com>
2017-07-17 11:07:58 +0800

    save in file

--- pyTest.py
+++ pyTest.py
@@ -1,5 +1,10 @@
 import os
-a=os.system("git log")
-
-print a
-
+au=[]
+#a=os.system("git log -3 >>a.txt")
+author=os.popen('''git log -3 --pretty="%H" ''')
+au=author.read().splitlines()
+print au
+f=file("log.txt","w")
+for a in au:
+	f.write(a+"\n")
+f.close()
